adf5bebd669cbe7baf8d89dfdd1c5b9b6880b38c
Simran-Sahni <sahnisimran58@gmail.com>
2020-03-20 21:14:09 +0530

    upload class files

--- Pilot.java
+++ Pilot.java
@@ -1,12 +1,20 @@
+import java.util.*;
+import java.io.*;
 
 public class Pilot 
 {
 	public static void main(String[] args) 
 	{
 		String arr[] = {"A","B","C","D","E"};
-		Relation rel = new Relation(arr);
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		//n is the number of FDs to be entered
+		Relation rel = new Relation(arr,n);
 		System.out.println(rel);
+		int num=rel.length();
 		
+
+
 		String fdtest = "{A,B} -> {C,D,E}";
 		FunDep fd = new FunDep(fdtest,rel);
 		System.out.println(fd);
--- Relation.java
+++ Relation.java
@@ -1,16 +1,21 @@
+import java.util.*;
 public class Relation 
 {
+	Scanner sc=new Scanner(System.in);
 	Relation parent;
 	String name;
 	public Attribute[] atr_List;
 	public Attribute[] prim_key;
+	//let n be the number of dependancies to be entered;
+	public FunDep[] fds;
 	public int NF;
 	// Construct New Relation
-	public Relation(String[] rel)
+	public Relation(String[] rel,int n)
 	{
 		atr_List = toAttribute(rel);
 		parent = null;
 		name = "DEFAULT";
+		fds=getFDs(n);
 		// TODO Functional Dependencies
 		prim_key = findPK();
 		NF = findNF();
@@ -31,15 +36,34 @@ public class Relation
 		}
 		return ret;		
 	}
-	String[] getFDs() 
+	
+	FunDep[] getFDs(int n) 
 	{
-		String ret[] = new String[5];
+		FunDep ret[] = new FunDep[n];
+		int i=0;
+		while(n>=0)
+		{
+			ret[i].rep=sc.nextLine();
+			i++;
+			n--;
+		}
+		
 		return ret;
 	}
+	//pass to this function the set of FDs in this relation
+	//and get the minimum NF of the relation
 	private int findNF() 
 	{
-		return 1;
+
+		int min=500;
+		for(int i=0;i<fds.length;i++)
+		{
+			min=min<=fds[i].NF ?min: fds[i].NF;
+		}
+		return min;
+		
 	}
+    
 	private Attribute[] findPK()
 	{
 		Attribute ret[] = new Attribute[5];
