33a7f7f6e85379b856737b3aacf37ceda2869acb
Radhesh Sarma <radheshsarma29@gmail.com>
2020-03-21 21:45:35 +0530

    Work by Radhesh

--- FunDep.java
+++ FunDep.java
@@ -1,11 +1,20 @@
-
+import java.util.*;
 public class FunDep 
 {
 	public int NF;
 	String rep;
+	String rep1[]=new String[2];
 	public FunDep(String str, Relation rel) 
 	{
 		rep = str;
+		StringTokenizer stt=new StringTokenizer(rep,"->");
+		
+		int t=0;
+		//for now assumed that FD is simply tokenized by ->
+		while(stt.hasMoreTokens())
+		   rep1[t++]=stt.nextToken();
+
+		//someone please address the below
 		// to extract attributes and work with them,
 		// tokenize "{A,B} -> {C,D,E}" (str) with delimiters ',' , '{', and '}' 
 		NF = findNF();
@@ -14,20 +23,26 @@ public class FunDep
 	//Check if a relation is in the second normal form (2NF)
 	boolean check2NF()
 	{
+		if(isPrimaryKey(rep1[0]))
 		return true;
+		return false;
 	}
 
 	//Check if a relation is in the third normal form (3NF)
 	boolean check3NF()
 	{
+		if(isPrimaryKey(rep1[0])||(isPrimaryKey(rep1[1])))
 		return true;
+		return false;
 	}
 
 
 	//Check if a relation is in Boyce-Codd normal form (BCNF)
 	boolean checkBCNF()
 	{
+		if(isPrimaryKey(rep1[0]))
 		return true;
+		return false;
 	}
 
 	// returns the normal form of the given relation 
@@ -37,6 +52,9 @@ public class FunDep
 	//	returns 4 if relation is in the Boyce-Codd normal form (BCNF)
 	int findNF() 
 	{
+		if(checkBCNF()) return 0;
+		if(check3NF()) return 3;
+		if(check2NF()) return 2;
 		return 1;
 	}
 	/*
